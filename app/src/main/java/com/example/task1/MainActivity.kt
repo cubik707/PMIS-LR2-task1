4c8513e9df2fe6ca4ade3a9f7bd463fda741570e
Vladislava <stdemidovez@gmail.com>
2024-09-18 21:04:18 +0300

    add users

--- app/src/main/java/com/example/task1/MainActivity.kt
+++ app/src/main/java/com/example/task1/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,9 +17,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,53 +32,55 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+sealed class UserRoutes(val route: String) {
+    object Users : UserRoutes("users")
+    object User : UserRoutes("user")
+}
 @Composable
 fun Main() {
     val navController = rememberNavController()
-    Column(Modifier.padding(8.dp)) {
-        NavBar(navController = navController)
-        NavHost(navController, startDestination = NavRoutes.Home.route) {
-            composable(NavRoutes.Home.route) { Home() }
-            composable(NavRoutes.Contacts.route) { Contacts() }
-            composable(NavRoutes.About.route) { About() }
+    val employees = listOf(
+        Employee(1, "Tom", 39),
+        Employee(2, "Bob", 43),
+        Employee(3, "Sam", 28)
+    )
+    NavHost(navController, startDestination = UserRoutes.Users.route) {
+        composable(UserRoutes.Users.route) { Users(employees, navController) }
+        composable(UserRoutes.User.route + "/{userId}",
+            arguments = listOf(navArgument("userId") { type = NavType.IntType }))
+        {
+                stackEntry ->
+            val userId = stackEntry.arguments?.getInt("userId")
+            User(userId, employees)
         }
     }
 }
 @Composable
-fun NavBar(navController: NavController){
-    Row(
-        Modifier.fillMaxWidth().padding(bottom = 8.dp)){
-        Text("Home",
-            Modifier
-                .weight(0.33f)
-                .clickable { navController.navigate(NavRoutes.Home.route) },
-            fontSize = 22.sp, color= Color(0xFF6650a4))
-        Text("Contacts",
-            Modifier
-                .weight(0.33f)
-                .clickable { navController.navigate(NavRoutes.Contacts.route) },
-            fontSize = 22.sp, color= Color(0xFF6650a4))
-        Text("About",
-            Modifier
-                .weight(0.33f)
-                .clickable { navController.navigate(NavRoutes.About.route) },
-            fontSize = 22.sp, color= Color(0xFF6650a4))
+fun Users(data: List<Employee>, navController: NavController){
+    LazyColumn {
+        items(data){
+                u->
+            Row(Modifier.fillMaxWidth()){
+                Text(u.name,
+                    Modifier.padding(8.dp).clickable {
+                        navController.navigate("user/${u.id}") },
+                    fontSize = 28.sp)
+            }
+        }
     }
 }
 @Composable
-fun Home(){
-    Text("Home Page", fontSize = 30.sp)
+fun User(userId:Int?, data: List<Employee>){
+    val user = data.find { it.id==userId }
+    if(user!=null) {
+        Column {
+            Text("Id: ${user.id}", Modifier.padding(8.dp), fontSize = 22.sp)
+            Text("Name: ${user.name}", Modifier.padding(8.dp), fontSize = 22.sp)
+            Text("Age: ${user.age}", Modifier.padding(8.dp), fontSize = 22.sp)
+        }
+    }
+    else{
+        Text("User Not Found")
+    }
 }
-@Composable
-fun Contacts(){
-    Text("Contact Page", fontSize = 30.sp)
-}
-@Composable
-fun About(){
-    Text("About Page", fontSize = 30.sp)
-}
-sealed class NavRoutes(val route: String) {
-    object Home : NavRoutes("home")
-    object Contacts : NavRoutes("contact")
-    object About : NavRoutes("about")
-}
+data class Employee(val id:Int, val name:String, val age:Int)
